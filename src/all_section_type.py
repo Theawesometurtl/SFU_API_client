f01ac01360a021bafd7f3e20ad8acef315cf5e14
KianKhadempour <kiankhadempour@gmail.com>
2024-09-27 14:42:20 -0700

    Finish all_section_type

--- src/all_section_type.py
+++ src/all_section_type.py
@@ -1,28 +1,73 @@
-import requests
+import asyncio
+import pickle
+from pprint import pprint
+
+import httpx
+
 base_url = "http://www.sfu.ca/bin/wcm/course-outlines?"
 
-# blacklisted_departments = ['arab', 'bot', 'bus', ];
-
 api_url = base_url + "current/current"
-departments = requests.get(api_url).json()
+
+departments = [dept["value"] for dept in httpx.get(api_url).json()]
+
+# course_sections = [
+#     requests.get(f"{api_url}/{dept}/{course['value']}").json()
+#     for dept in departments
+#     for course in requests.get(f"{api_url}/{dept}").json()
+# ]
 
 
-total_departments = [dept['value'] for dept in departments]
-whitelisted_departments = ['chem', 'cmpt', 'cogs']
-# whitelisted_departments = [dept for dept in total_departments if dept not in blacklisted_departments]
+async def main():
+    # course_sections: list[Any] = []
+    # async with httpx.AsyncClient() as client:
+    #     for dept in tqdm(departments):
+    #         courses_r = await client.get(f"{api_url}/{dept}")
+    #         try:
+    #             courses = courses_r.json()
+    #         except Exception as e:
+    #             print(e)
+    #             print(dept)
+    #             print(courses_r.content.decode())
+    #             continue
 
-# course_numbers = requests.get(f"{api_url}/{'econ'}").json()
-course_numbers = [requests.get(f"{api_url}/{dept}").json() for dept in whitelisted_departments]
+    #         tasks: list[Coroutine[Any, Any, httpx.Response]] = []
+    #         for course in tqdm(courses):
+    #             if course == "errorMessage":
+    #                 continue
+    #             try:
+    #                 tasks.append(client.get(f"{api_url}/{dept}/{course['value']}"))
+    #             except Exception as e:
+    #                 print(e)
+    #                 print(dept)
+    #                 print(course)
 
-# course_time = [requests.get(f"{api_url}/{dept}").json() for course in course_numbers['value'] if course_numbers['value'] < 600]
+    #         course_sections_responses = await asyncio.gather(*tasks)
+
+    #         for course_sections_response in tqdm(course_sections_responses):
+    #             try:
+    #                 course_sections.append(course_sections_response.json())
+    #             except Exception as e:
+    #                 print(e)
+
+    # with open("course_sections.pkl", "wb") as f:
+    #     pickle.dump(course_sections, f)
+
+    with open("course_sections.pkl", "rb") as f:
+        course_sections = pickle.load(f)
+
+    section_codes: set[str] = set()
+    for course_section in course_sections:
+        for course in course_section:
+            if "errorMessage" in course:
+                continue
+            try:
+                section_codes.add(course["sectionCode"])
+            except Exception as e:
+                print(e)
+                print(course_section)
+                raise ValueError
+
+    pprint(list(section_codes))
 
 
-print(departments)
-print(course_numbers)
-
-with open('file2.txt', 'w') as f:
-    for dept in course_numbers:
-        for sfu_class in dept:
-            print(sfu_class)
-            f.write(sfu_class["value"] + " " + sfu_class["title"])
-            f.write('\n')
+asyncio.run(main())
